58d60736cd32606143ed9881bf4b114626036706
Eric Lindemann <elindemann3@gmail.com>
2017-04-05 15:10:38 -0600

    added callback from floatinglist to analysis page

--- app/assets/javascripts/components/analyses.js.jsx
+++ app/assets/javascripts/components/analyses.js.jsx
@@ -2,16 +2,22 @@ class Analysis extends React.Component {
   constructor(props) {
     super(props);
     this.state = {data: this.props.data};
-    
+    this.handleClickList1 = this.handleClickList1.bind(this);
   }
 
+
+  handleClickList1(word1) {
+    console.log(word1);
+  }
 
 
   render() {
     return (
       <div className = 'analysismain'>
         <TopBar />
-        <FloatingList data = {this.state.data} />
+        <FloatingList 
+          data = {this.state.data} 
+          onWordClick = {this.handleClickList1}  />
         <FloatingList data = {this.state.data} />
       </div>
     );
--- app/assets/javascripts/components/floatinglist.js.jsx
+++ app/assets/javascripts/components/floatinglist.js.jsx
@@ -2,21 +2,32 @@ class FloatingList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {data: this.props.data};
+    this.handleChange = this.handleChange.bind(this);   
     
   }
 
-    render() {
-    const words = this.state.data;
-    const listItems = words.map((words) =>
-      <ul key={words.toString()} >
-        <a className = 'floatingList' href = '/'>
-          {words}
-        </a>
-      </ul>
-    );
+
+  handleChange(e) {
+    this.props.onWordClick(e.target.getAttribute('value'));
+  }
+
+  render() {
+    words = this.props.data
     return (
-      <div className = 'floatinglist' id= 'firstWords'>
-          {listItems}
+      <div className = 'floatinglist'>
+        <ul>
+          {words.map((word) =>
+            <li key={word.toString()}>
+              <a 
+                value={word}
+                onClick = {this.handleChange}> 
+                {word}
+              
+              </a>
+            </li>
+            
+          )}
+        </ul>      
       </div>
     );
   }
